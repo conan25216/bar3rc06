e8f00a9340180663898c9508ed0655648b8af586
conan <conan25216@gmail.com>
2019-01-15 23:51:12 +0800

    Update conan_test.py

--- cif/hunter/conan_test.py
+++ cif/hunter/conan_test.py
@@ -36,7 +36,7 @@ class VT_hunter(object):
                   'resource': md5
                   }
         try:
-            # proxies = {'https': 'https://web-proxy.oa.com:8080',}
+            # proxies = {'https': 'https://**.com:8080',}
             # response = requests.get(url, params=params, proxies=proxies)
             response = requests.get(url, params=params, timeout=10)
             data = response.json()
@@ -90,7 +90,7 @@ class VT_hunter(object):
         file_data = {}
 
         try:
-            # proxies = {'https': 'https://web-proxy.oa.com:8080',}
+            # proxies = {'https': 'https://*.*.com:8080',}
             # response = requests.get(url, params=params, proxies=proxies)
             response = requests.get(url, params=params)
             data = response.json()
@@ -114,16 +114,7 @@ class VT_hunter(object):
     def search_VT_data(self, ioc):
         num = random.randrange(0, 9)
         token = [
-            'd3814579e40f84c059a76ff58e964de4f32ebd6c9282c7abb49310ad1dc7bf33',
-            'a587a065f16af85901522f84a11af4e1d818d69ff711e67660d808ea8d022ba5',
-            '18f58ddc96df022552f10a0d1ba6c04e882f2e6ef34bf2c2802622c119b8a3ba',
-            '72651dac6e21bf477da28ec67a12b9c19420ad3d31c77b713c2afd93914db9dd',
-            '29b085d53489c17313c531c108982c8c8a02c91092e421569e3f96f49b63217d',
-            '46a84a65aefcc5e838ce6b24a6b92c715702988e05141745ea33be2fd8650af2',
-            '08bd5498bb6c73e02f376a8e073fd78e912bd0386f968fd2c86683e445a4f2b2',
-            'a1a8203872ff17201f261241d44b4dccf46176790533d23cc5b6f445cb0e07a0',
-            '0a07c392c2abd33206cc23474e60b9c3b8dc9a5751146a600fd71fc53f416fa9',
-            '34fd4514043d404f39b1a75747314638af782726ef248912d6784bb8896c6d51'
+         ""
         ]
         try:
             if re.search(
